5006256f8c141609fd9a8dd4a36e124770eccbc5
Sharan Thakur <sharandt19@live.com>
2024-07-02 15:41:19 +0530

    Initial Commit

---
+++ Pokemons/PokemonsApp.swift
@@ -0,0 +1,33 @@
+//
+//  PokemonsApp.swift
+//  Pokemons
+//
+//  Created by Sharan Thakur on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonsApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
